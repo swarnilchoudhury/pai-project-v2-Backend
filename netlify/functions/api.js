57c5f7012daf12eead0125427af169e278791dd1
swarnilchoudhury <choudhury.swarnil1999@gmail.com>
2024-06-01 00:40:56 +0530

    Initial changes

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,11 +1,28 @@
-import express, { Router } from "express";
-import serverless from "serverless-http";
+const express = require("express");
+const serverless = require("serverless-http");
 
-const api = express();
+// Create an instance of the Express app
+const app = express();
 
-const router = Router();
-router.get("/hello", (req, res) => res.send("Hello World!"));
+// Create a router to handle routes
+const router = express.Router();
 
-api.use("/api/", router);
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+})
+// Define a route that responds with a JSON object when a GET request is made to the root path
+router.get("/", (req, res) => {
 
-export const handler = serverless(api);
+  res.json({ name: "Hello", Age: 19 });
+
+});
+
+// Use the router to handle requests to the `/.netlify/functions/api` path
+app.use("/api/", router);
+
+// Export the app and the serverless function
+module.exports = app;
+module.exports.handler = serverless(app);
