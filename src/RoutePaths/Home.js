770c36c5bdfe154d2373428248fb58f6b27123c9
swarnilchoudhury <choudhury.swarnil1999@gmail.com>
2024-08-04 23:40:43 +0530

    API Changes

--- src/RoutePaths/Home.js
+++ src/RoutePaths/Home.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const config = require("../../config/config.json");
-const { db, currentTime } = require('../credentials/firebaseCredentials');
+const { db } = require('../credentials/firebaseCredentials');
 const { verifyIdToken } = require('../authMiddleware');
 
 router.use(verifyIdToken);
@@ -10,43 +10,35 @@ router.use(verifyIdToken);
 router.get("/Home", async (req, res) => {
 
     try {
-        let homePageDataArray = [];
         let docRef;
         let status = req.headers['x-status'];
 
         if (status === 'Deactive') {
-            docRef = db.collection(config.Collections.StudentDetailsDeactiveStatus);
+            docRef = db.collection(config.Collections.StudentDetailsDeactiveStatus).orderBy('StudentName', 'asc');
         }
         else if (status === 'Unapproval') {
-            docRef = db.collection(config.Collections.StudentDetailsApprovalStatus);
+            docRef = db.collection(config.Collections.StudentDetailsApprovalStatus).orderBy('StudentName', 'asc');
         }
         else {
-            docRef = db.collection(config.Collections.StudentDetailsActiveStatus);
+            docRef = db.collection(config.Collections.StudentDetailsActiveStatus).orderBy('StudentName', 'asc');
         }
 
         const snapshot = await docRef.get();
-        snapshot.forEach(doc => {
-            const data = doc.data();
-            if (data.CreatedDateTime) {
-                let createdDateTime = doc.data().CreatedDateTime.toDate().toLocaleString("en-US", {
-                    year: "numeric",
-                    month: "short",
-                    day: "numeric",
-                    hour: "2-digit",
-                    minute: "2-digit",
-                    second: "2-digit"
-                });
 
-                data.CreatedDateTime = createdDateTime;
-            }
-            homePageDataArray.push(data)
+        // Map the snapshot to an array of document data
+        let homePageDataArray = snapshot.docs.map(doc => {
+            const data = doc.data();
+            const { CreatedDateTime, ...otherData } = data; // Destructure to exclude createdDateTime
+            return {
+                ...otherData,
+            };
         });
 
-        res.json(homePageDataArray);
+        return res.json(homePageDataArray);
     }
     catch (e) {
         console.log(e)
-        res.sendStatus(400);
+        return res.sendStatus(400);
     }
 }
 );
@@ -55,22 +47,22 @@ router.get("/Home", async (req, res) => {
 router.post("/searchCode", async (req, res) => {
 
     try {
-        let studentCode = req.body.studentCode;
+        let StudentCode = req.body.StudentCode;
 
-        if (!studentCode.includes("PAI")) {
-            studentCode = "PAI-" + studentCode;
+        if (!StudentCode.includes("PAI")) {
+            StudentCode = "PAI-" + StudentCode;
         }
 
-        const docRef = db.collection(config.Collections.StudentDetailsActiveStatus).doc(studentCode);
+        const docRef = db.collection(config.Collections.StudentDetailsActiveStatus).doc(StudentCode);
         const doc = await docRef.get();
         if (doc.exists) {
             return res.json({ returnCode: 1 });
         }
 
-        res.json({ returnCode: 0 })
+        return res.json({ returnCode: 0 })
     }
     catch {
-        res.sendStatus(400);
+        return res.sendStatus(400);
     }
 }
 );
--- src/RoutePaths/Role.js
+++ src/RoutePaths/Role.js
@@ -31,35 +31,45 @@ router.post("/create", async (req, res) => {
 
     try {
         let requestBody = req.body;
-        let studentCode = requestBody.studentCode;
+        let StudentCode = requestBody.StudentCode;
 
-        if (!studentCode.includes("PAI")) {
-            studentCode = "PAI-" + studentCode;
+        if (!StudentCode.includes("PAI")) {
+            StudentCode = "PAI-" + StudentCode;
         }
 
-        let document = { ...requestBody, studentCode: studentCode, CreatedDateTime: currentTime }
+        let createdDateTimeFormatted = new Date().toLocaleString("en-US", {
+            year: "numeric",
+            month: "short",
+            day: "numeric",
+            hour: "2-digit",
+            minute: "2-digit",
+            second: "2-digit"
+        });
+
+        let document = { ...requestBody, StudentCode: StudentCode, CreatedDateTime: currentTime, CreatedDateTimeFormatted: createdDateTimeFormatted };
+
 
         if (req.Role.toUpperCase() === "ADMIN") {
 
-            let docRef = db.collection(config.Collections.StudentDetailsActiveStatus).doc(studentCode);
+            let docRef = db.collection(config.Collections.StudentDetailsActiveStatus).doc(StudentCode);
             await docRef.set(document);
 
             // Confirm the document was written successfully
             let doc = await docRef.get();
             if (doc.exists) {
-                return res.status(200).json({ message: studentCode + ' has been created.' });
+                return res.status(200).json({ message: StudentCode + ' has been created.' });
             } else {
                 return res.status(500).json({ message: 'Failed to write document' });
             }
         }
         else {
-            let docRef = db.collection(config.Collections.StudentDetailsApprovalStatus).doc(studentCode);
-            await docRef.set(requestBody);
+            let docRef = db.collection(config.Collections.StudentDetailsApprovalStatus).doc(StudentCode);
+            await docRef.set(document);
 
             // Confirm the document was written successfully
             let doc = await docRef.get();
             if (doc.exists) {
-                return res.status(200).json({ message: studentCode + ' has been sent for approval.' });
+                return res.status(200).json({ message: StudentCode + ' has been sent for approval.' });
             } else {
                 return res.status(500).json({ message: 'Failed to write document' });
             }
