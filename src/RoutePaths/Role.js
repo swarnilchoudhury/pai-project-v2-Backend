d6c336488f173cafe4369de9fb3fdffd9ecab809
swarnilchoudhury <choudhury.swarnil1999@gmail.com>
2024-08-08 01:19:27 +0530

    Acttive, Deactive Status implementation

--- src/RoutePaths/Role.js
+++ src/RoutePaths/Role.js
@@ -81,6 +81,63 @@ router.post("/create", async (req, res) => {
         return res.send({ "responseCode": 0 }).status(400);
     }
 }
+
+
+);
+
+//Update new documents
+router.post("/update", async (req, res) => {
+
+    try {
+        if (req.Role.toUpperCase() === 'ADMIN') {
+            let status = req.headers['x-update'].toLowerCase();
+            let currentDocRef, newDocRef;
+            if (status === 'deactive') {
+                currentDocRef = db.collection(config.Collections.StudentDetailsActiveStatus);
+                newDocRef = db.collection(config.Collections.StudentDetailsDeactiveStatus);
+            }
+            else if(status === 'active'){
+                currentDocRef = db.collection(config.Collections.StudentDetailsDeactiveStatus);
+                newDocRef = db.collection(config.Collections.StudentDetailsActiveStatus);
+            }
+
+            const movePromises = req.body.data.map(
+                async studentCode => {
+                    let docRef = currentDocRef.doc(studentCode);
+
+                    // Get the document
+                    const docSnapshot = await docRef.get();
+
+                    // Get the document data
+                    const docData = docSnapshot.data();
+
+                    // Reference to the new document location in the target collection
+                    const newDocumentRef = newDocRef.doc(studentCode);
+
+                    // Write the document data to the new collection
+                    await newDocumentRef.set(docData);
+
+                    // Delete the document from the original collection
+                    await docRef.delete();
+
+                }
+            );
+
+            // Wait for all move operations to complete
+            await Promise.all(movePromises);
+
+            return res.sendStatus(200);
+        }
+        else {
+            return res.json({ message: "Not Authorized" });
+        }
+
+    }
+    catch (e) {
+        console.log(e);
+        return res.sendStatus(400);
+    }
+}
 );
 
 module.exports = router;
